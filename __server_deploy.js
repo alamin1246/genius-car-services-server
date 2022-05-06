41ce78b9b37395c920d5dec51b9ca5017d25ca9d
alamin1246 <hossainalamin1246@gmail.com>
2022-05-06 21:25:16 +0600

    added hero end point

---
+++ __server_deploy.js
@@ -0,0 +1,32 @@
+/* 
+*------------------------
+* ONE TIME for your computer
+* ------------------------
+* 1. create heroku account
+* 2. verify email
+* 3. install heroku cli
+* 4. heroku login
+* ---------------------------
+* For each project one time
+* --------------------------
+* 1. heroku create
+* 2. make sure you: git add . git commit. git push
+* 3. git push heroku main
+* 4. Go to Heroku Dashboard > Current Project > Setting > Reveal Config vars
+* 5. copy paste config vars from your .env file
+* 6. Make sure you have whitelisted all ip address to access mongodb
+* -----------------------------
+* UPDATE SERVER with new changes
+* -----------------------------
+* 1. Make Changes
+* 2.make sure you: git add . git commit. git push
+* 3. git push heroku main
+* -----------------------------
+* Connect Server with Client
+* -----------------------------
+* 1.
+* 2.
+* 3.
+* 4.
+* 5.
+*/
--- index.js
+++ index.js
@@ -115,6 +115,10 @@ app.get('/', (req, res) => {
   res.send('Running Genius Car Server');
 });
 
+app.get('/hero', (req, res) => {
+  res.send('Hero meets hero ku')
+})
+
 app.listen(port, () => {
   console.log('Listen to port 5000');
 });
